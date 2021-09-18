729de9635b18deaedb619557f5ecf136aea78c2c
Vishal <44179247+sumanptech@users.noreply.github.com>
2021-09-18 19:12:07 +0100

    started setting up logic ground

---
+++ PromotionEngine.BLL/BusinessClass/IndividualSKUPromotions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using PromotionEngine.Entity;
+
+namespace PromotionEngine.BLL
+{
+    public class IndividualSKUPromotions: ISKUPromotions
+    {
+        public double FetchSKUPrice(List<SKUCartEntity> objCart, PromotionTypeEntity objReturnProduct)
+        {
+            throw new NotImplementedException();
+        }
+
+       public bool IsApplicable(SKUCartEntity objCart, List<PromotionTypeEntity> promotionList, out PromotionTypeEntity objReturnProduct)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ PromotionEngine.BLL/BusinessInterface/ISKUPromotions.cs
@@ -0,0 +1,16 @@
+﻿using PromotionEngine.Entity;
+using System.Collections.Generic;
+
+namespace PromotionEngine.BLL
+{
+    /// <summary>
+    /// Interface declaring operation for different SKU Promotions
+    /// A class can Implement it basis to new Promotion Type
+    /// </summary>
+    public interface ISKUPromotions
+    {
+        double FetchSKUPrice(List<SKUCartEntity> objCart, PromotionTypeEntity objReturnProduct);
+
+        bool IsApplicable(SKUCartEntity objCart, List<PromotionTypeEntity> promotionList, out PromotionTypeEntity objReturnProduct);
+    }
+}
