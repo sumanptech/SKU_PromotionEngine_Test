6870fa9f7bfd85b9aedbcddae51b4ae5d27c167b
Vishal <44179247+sumanptech@users.noreply.github.com>
2021-09-18 17:47:14 +0100

    TDD Approach

--- Test.PromotionEngine/TestPromotionEngine.cs
+++ Test.PromotionEngine/TestPromotionEngine.cs
@@ -20,14 +20,10 @@ namespace Test.PromotionEngine
 
 
 
-        //Scenario 0
-        //0	* A	50
-        //0	* B	30
-        //0	* C	20
-        //Total 0
-
+        // Deafult Testcase
+        //0	* A	50        //0	* B	30        //0	* C	20        //Total 0
         [TestMethod]
-        public void Scenario_NoSKUSelected()
+        public void NoSKUSelectedTest()
         {
             objCart = new List<SKUCartEntity>
             {
@@ -38,6 +34,69 @@ namespace Test.PromotionEngine
             objSKUs = new ProcessSKUCart();
 
             double expectedResult = 0;
+            double actualResult = objSKUs.ApplyPromotionsOnSKUs(objCart, promotionList).CheckoutPrice;
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+
+
+
+        //Scenario A
+        //1	* A	50      //1	* B	30          //1	* C	20         //Total 100
+
+        [TestMethod]
+        public void ABC_OneUnitEachSelectedTest()
+        {
+            objCart = new List<SKUCartEntity>
+            {
+                new SKUCartEntity{ SKU_ID = "A", SKU_Unit = 1, SKU_DefaultPrice = 50 },
+                new SKUCartEntity{ SKU_ID = "B", SKU_Unit = 1, SKU_DefaultPrice = 30 },
+                new SKUCartEntity{ SKU_ID = "C", SKU_Unit = 1, SKU_DefaultPrice = 20 }
+            };
+
+            objSKUs = new ProcessSKUCart();
+            double expectedResult = 100;
+            double actualResult = objSKUs.ApplyPromotionsOnSKUs(objCart, promotionList).CheckoutPrice;
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+
+        //Scenario B
+        //5 * A	 130 + 2*50      //5 * B		45 + 45 + 30     //1 * C		28 
+        //Total	370
+        [TestMethod]
+        public void SKU_5A_5B_1C_SelectedTest()
+        {
+            objCart = new List<SKUCartEntity>
+            {
+                new SKUCartEntity { SKU_ID = "A", SKU_Unit = 5, SKU_DefaultPrice = 50 },
+                new SKUCartEntity { SKU_ID = "B", SKU_Unit = 5, SKU_DefaultPrice = 30 },
+                new SKUCartEntity { SKU_ID = "C", SKU_Unit = 1, SKU_DefaultPrice = 20 }
+            };
+
+            objSKUs = new ProcessSKUCart();
+            double expectedResult = 370;
+            double actualResult = objSKUs.ApplyPromotionsOnSKUs(objCart, promotionList).CheckoutPrice;
+            //Assert
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        //Scenario C
+        //3	* A	130        //5	* B	45 + 45 + 1 * 30        //1	* C	-        //1	* D	30
+        //Total	280
+        [TestMethod]
+        public void SKU_3A_5B_1C_1D_SelectedTest()
+        {
+            objCart = new List<SKUCartEntity>
+            {
+                new SKUCartEntity { SKU_ID = "A", SKU_Unit = 3, SKU_DefaultPrice = 50 },
+                new SKUCartEntity { SKU_ID = "B", SKU_Unit = 5, SKU_DefaultPrice = 30 },
+                new SKUCartEntity { SKU_ID = "C", SKU_Unit = 1, SKU_DefaultPrice = 20 },
+                new SKUCartEntity { SKU_ID = "D", SKU_Unit = 1, SKU_DefaultPrice = 15}
+            };
+
+            objSKUs = new ProcessSKUCart();
+            double expectedResult = 280;
             double actualResult = objSKUs.ApplyPromotionsOnSKUs(objCart, promotionList).CheckoutPrice;
             Assert.AreEqual(expectedResult, actualResult);
         }
