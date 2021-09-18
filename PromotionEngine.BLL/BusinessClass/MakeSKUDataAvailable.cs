e04f668454d5b351348c7ec9bfc92836207af528
Vishal <44179247+sumanptech@users.noreply.github.com>
2021-09-18 19:05:56 +0100

    Business Logic

--- PromotionEngine.BLL/BusinessClass/MakeSKUDataAvailable.cs
+++ PromotionEngine.BLL/BusinessClass/MakeSKUDataAvailable.cs
@@ -1,14 +1,15 @@
-﻿using System;
+﻿using PromotionEngine.DataLayer;
+using PromotionEngine.Entity;
+using PromotionEngine.Logging;
+using System;
 using System.Collections.Generic;
-using System.Text;
-using PromotionEngine.DataLayer;
 
 namespace PromotionEngine.BLL
 {
     /// <summary>
     /// Class defining operation To load SKU and Promotion Data from DataLayer
     /// </summary>
-   public class MakeSKUDataAvailable
+    public class MakeSKUDataAvailable
     {
         LoadSKUData objData;
 
@@ -18,6 +19,43 @@ namespace PromotionEngine.BLL
         public MakeSKUDataAvailable()
         {
             objData = new LoadSKUData();
+        }
+
+        /// <summary>
+        /// Load All SKUs
+        /// </summary>
+        /// <returns></returns>
+        public List<SKUEntity> GetAvilableSKUs()
+        {
+            List<SKUEntity> objSKU = new List<SKUEntity>();
+            try
+            {
+
+                objSKU = objData.GetAllSKUs();
+            }
+            catch (Exception ex)
+            {
+                LogInfo.LogMessage("Errror while getting available SKUs #GetAvilableSKUs#: " + ex.Message);
+            }
+            return objSKU;
+        }
+
+        /// <summary>
+        /// Reads all available promotion types from data layer
+        /// </summary>
+        /// <returns>List of available promotion types</returns>
+        public List<PromotionTypeEntity> GetAvailablePromotionTypes()
+        {
+            List<PromotionTypeEntity> objPromotionTypes = new List<PromotionTypeEntity>();
+            try
+            {
+                objPromotionTypes = objData.GetAllAvailablePromotionTypes();
+            }
+            catch (Exception ex)
+            {
+                LogInfo.LogMessage("Errror while getting available Promotion Types #GetAvailablePromotionTypes#: " + ex.Message);
+            }
+            return objPromotionTypes;
         }
     }
 }
